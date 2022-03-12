Fix bugs in lookahead policy and add test for it.

code/dqn_emb_policy.py
@@ -113,11 +113,14 @@ class DQNEmb(nn.Module):
         #     nn.Linear(self.args.hidden_size, self.args.hidden_size) for _ in range(self.args.num_hidden - 1)])
         
         # self._q_out = nn.Linear(self.args.hidden_size, 1)
-        self._q_adv_1 = nn.Linear(self.args.hidden_size, self.args.hidden_size)
-        self._q_adv_2 = nn.Linear(self.args.hidden_size, 1)
+        self._q_out_1 = nn.Linear(self.args.hidden_size, self.args.hidden_size)
+        self._q_out_2 = nn.Linear(self.args.hidden_size, 1)
         
-        self._q_val_1 = nn.Linear(self.args.hidden_size, self.args.hidden_size)
-        self._q_val_2 = nn.Linear(self.args.hidden_size, 1)
+        # self._q_adv_1 = nn.Linear(self.args.hidden_size, self.args.hidden_size)
+        # self._q_adv_2 = nn.Linear(self.args.hidden_size, 1)
+        
+        # self._q_val_1 = nn.Linear(self.args.hidden_size, self.args.hidden_size)
+        # self._q_val_2 = nn.Linear(self.args.hidden_size, 1)
 
          
 
@@ -209,36 +212,22 @@ class DQNEmb(nn.Module):
         # for hidden_fc in self.hidden_fcs:
         #     x = F.gelu(hidden_fc(x))
         
-        adv = F.gelu(self._q_adv_1(state_embs[:, :inv_embs.shape[1]]))
-        adv = self._q_adv_2(adv)
+        # adv = F.gelu(self._q_adv_1(state_embs[:, :inv_embs.shape[1]]))
+        # adv = self._q_adv_2(adv)
 
-        val = F.gelu(self._q_val_1(state_embs[:, -1]))
-        val = self._q_val_2(val)
-        #print("adv.shape")
-        # print("adv.shape", adv.shape)
-        # print("val.shape", val.shape)
-        # print("adv.squeeze(-1).shape", adv.squeeze(-1).shape)
-        # print("adv.mean(1).shape", adv.mean(1).shape)
-        # print("adv.mean(1)", adv.mean(1))
-        
-        # print("adv.mean(1):", adv.mean(1))
-        # print("adv.squeeze(-1)", adv.squeeze(-1))
-        # print("adv.squeeze(-1) - adv.mean(1)", adv.squeeze(-1) - adv.mean(1), "\n")
-        # print("val", val)
-        # print("adv.squeeze(-1)", adv.squeeze(-1))
-        # print("adv.mean(1)", adv.mean(1))
+        # val = F.gelu(self._q_val_1(state_embs[:, -1]))
+        # val = self._q_val_2(val)
 
-        num_valid = valid_actions_mask.sum(axis=1)
+        # num_valid = valid_actions_mask.sum(axis=1)
         
-        # adv_mean = torch.where(num_valid > 0, (valid_actions_mask * adv).sum(axis=1) / num_valid, num_valid)
-        adv_mean = (valid_actions_mask * adv).sum(axis=1) / valid_actions_mask.sum(axis=1)
+        # adv_mean = (valid_actions_mask * adv).sum(axis=1) / valid_actions_mask.sum(axis=1)
         
-        # Required as invalid actions for training when next state is None
-        adv_mean = torch.nan_to_num(adv_mean)
+        # # Required as invalid actions for training when next state is None
+        # adv_mean = torch.nan_to_num(adv_mean)
         
-        q_vals = val + adv.squeeze(-1) - adv_mean
-        #print("q_vals.shape", q_vals.shape)
-        #q_vals = self._q_out(x)
+        # q_vals = val + adv.squeeze(-1) - adv_mean
+        q_vals = F.gelu(self._q_out_1(state_embs[:, :inv_embs.shape[1]]))
+        q_vals = self._q_out_2(q_vals)
 
         if batch_size == 1:
             #print("q_vals.view(-1)", q_vals.view(-1))

code/dqn_policy.py
@@ -76,10 +76,10 @@ class DQNTrainer(RLPolicy):
 
     def _update_target(self):
         """Perform soft update of the target policy."""
-        for tgt_dqn_param, dqn_param in zip(self._dqn_target.parameters(), self._dqn.parameters()):
-            tgt_dqn_param.data.copy_(
-                self.args.tgt_tau * dqn_param.data + (1.0-self.args.tgt_tau) * tgt_dqn_param.data)
-        #self._dqn_target.load_state_dict(self._dqn.state_dict())
+        # for tgt_dqn_param, dqn_param in zip(self._dqn_target.parameters(), self._dqn.parameters()):
+        #     tgt_dqn_param.data.copy_(
+        #         self.args.tgt_tau * dqn_param.data + (1.0-self.args.tgt_tau) * tgt_dqn_param.data)
+        self._dqn_target.load_state_dict(self._dqn.state_dict())
 
     @property
     def epsilon_threshold(self):
@@ -296,6 +296,9 @@ class DQNTrainer(RLPolicy):
         target_vals = target_vals * next_state_mask
         td_targets = rewards + gammas * target_vals
 
+        # Force it to not assume Q-value rewards greater than the reward (as rewards are strictly negative)
+        td_targets = torch.clip(td_targets, max=rewards, min=-2.0 + rewards)
+
 
         # index used for getting the next nonempty next state
         # q_next_idx = 0
@@ -366,7 +369,7 @@ class DQNTrainer(RLPolicy):
         self._train_step += 1
 
         if self._train_step % self.args.tgt_update_step == 0:
-            #print("\nUPDATING TARGET\n")
+            print("\nUPDATING TARGET\n")
             self._update_target()
         
         # Print out q_values and td_targets for debugging/progress updates
@@ -376,9 +379,10 @@ class DQNTrainer(RLPolicy):
             print("q_values", q_values)
             print("td_targets", td_targets)
             print("loss", loss)
-            print("self._dqn._q_adv_1.weight.grad", self._dqn._q_adv_1.weight.grad.mean(), self._dqn._q_adv_1.weight.grad.min(), self._dqn._q_adv_1.weight.grad.max())
+            print("rewards", rewards)
+            #print("self._dqn._q_adv_1.weight.grad", self._dqn._q_adv_1.weight.grad.mean(), self._dqn._q_adv_1.weight.grad.min(), self._dqn._q_adv_1.weight.grad.max())
             print("self._dqn.inv_encoder._inv_emb_fc_1.weight.grad", self._dqn.inv_encoder._inv_emb_fc_1.weight.grad.mean(), self._dqn.inv_encoder._inv_emb_fc_1.weight.grad.min(), self._dqn.inv_encoder._inv_emb_fc_1.weight.grad.max())
-            print("is_ws:", is_ws, "\n")
+            # print("is_ws:", is_ws, "\n")
             # print("td_error", td_errors)
             # print("target_vals", target_vals)
             # print("td_error ** 2", td_errors ** 2)

code/evaluator.py
@@ -14,6 +14,7 @@ from naive_policy import NaivePolicy
 from random_policy import RandomPolicy
 from dqn_policy import DQNTrainer
 from dqn_emb_policy import DQNEmbTrainer
+from dqn_lookhead_policy import DQNLookaheadTrainer
 from primal_dual_policy import PrimalDual
 from actor_critic_policy import ActorCriticPolicy
 from visual import Visual
@@ -69,7 +70,7 @@ class Evaluator:
         self.dataset_sim = dataset_sim
 
         # Remove reward scaling
-        self.reward_man._reward_scale_factor = 1
+        #self.reward_man._reward_scale_factor = 1
 
         # Set the initial inventory
         self._inv_dict = self._init_inv()
@@ -171,6 +172,11 @@ class Evaluator:
                         elif "dqn_emb" in train_dict["policy_name"]:
                             policies[train_dict["policy_name"]] = DQNEmbTrainer(self.args, self.reward_man)
                             policies[train_dict["policy_name"]]._dqn.eval()
+                        elif "lookahead" in train_dict["policy_name"].lower():
+                            print("USING LOOKAHEAD")
+                            policies[train_dict["policy_name"]] = DQNLookaheadTrainer(self.args, self.reward_man)
+                            policies[train_dict["policy_name"]]._dqn.eval()
+
                         else:
                             raise Exception(f'Could not handle {train_dict["policy_name"]} policy!')
         return policies
@@ -191,6 +197,11 @@ class Evaluator:
         for i, policy_tuple in enumerate(eval_results.rewards_dict.items()):
             policy_name, rewards = policy_tuple
             avg_reward = sum(rewards) / len(rewards)
+            ret = 0
+
+            for i, reward in enumerate(rewards):
+                ret += reward * self.args.gamma ** i 
+            print("RETURN: ", ret)
             ax.bar(policy_name, -1 *avg_reward, label=policy_name, zorder=3)
             print(f"{policy_name} Average Reward: ", avg_reward)
         # Add legend 
@@ -245,9 +256,8 @@ class Evaluator:
 
                 for demand_node in demand_nodes:
                     # Get order results for policy
-                    if "dqn" in policy_name:
+                    if "dqn" in policy_name or "lookahead" in policy_name:
                         if self.dataset_sim is not None:
-                            
                             policy_results = policy(self.sim._inv_nodes, demand_node, self.dataset_sim.cur_sku_distr, argmax=True)
                         else:
                             policy_results = policy(self.sim._inv_nodes, demand_node, torch.tensor([1/self.args.num_skus]).repeat(self.args.num_skus).to(device), argmax=True)